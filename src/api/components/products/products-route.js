ab731c75fb8bafb5674c92eeca99d3cef6002a6c
eilleendwe <eilleenyeoh3@gmail.com>
2024-05-04 09:48:12 +0700

    [DEV] Membuat folder products

---
+++ src/api/components/products/products-route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const authenticationMiddleware = require('../../middlewares/authentication-middleware');
+const celebrate = require('../../../core/celebrate-wrappers');
+const productsControllers = require('./products-controller');
+const productsValidator = require('./products-validator');
+
+const route = express.Router();
+
+module.exports = (app) => {
+  app.use('/products', route);
+};
